7fd15018cabd7ef9e0e5a66340d6369b3c4dcff9
Bùi Vũ Thiên Ân (anbvt) <anbvt562003@gmail.com>
2025-06-26 02:40:30 +0900

    変更

--- src/util/CustomHook.ts
+++ src/util/CustomHook.ts
@@ -8,7 +8,7 @@ const useQuery = () => {
     return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 const useFetch = axios.create({
-    baseURL: "https://laptopecommercebe-production.up.railway.app"
+    baseURL: "http://localhost:8080"
 });
 
 export {
